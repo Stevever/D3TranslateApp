76c41b12e02bb4f360f31f15c6fa4046dd075d6a
Ryan Faulhaber <faulhaber.14@osu.edu>
2016-03-31 20:23:20 -0400

    NavigationDrawer added.

--- app/src/main/java/com/example/ryan/d3translationapp/MainActivity.java
+++ app/src/main/java/com/example/ryan/d3translationapp/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.ryan.d3translationapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NavigationDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        addLayout(R.layout.activity_main);
     }
 }
