1744c6cff4f876cdace75d9b160797b7784a11a0
Ryan Faulhaber <faulhaber.14@osu.edu>
2016-04-06 19:47:07 -0400

    Basic activities and fragments added.

---
+++ app/src/main/java/com/example/ryan/d3translationapp/ArticleActivity.java
@@ -0,0 +1,29 @@
+package com.example.ryan.d3translationapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ArticleActivity extends NavigationDrawerActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addLayout(R.layout.activity_article);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+			}
+		});
+	}
+
+}
---
+++ app/src/main/java/com/example/ryan/d3translationapp/ArticleListViewerActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.ryan.d3translationapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArticleListViewerActivityFragment extends Fragment {
+
+	public ArticleListViewerActivityFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_article_list_viewer, container, false);
+	}
+}
