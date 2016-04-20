14678041d44f34d3f8a4727a840218d42fc6e59f
joedonnelly9 <joedonnelly92@gmail.com>
2016-04-19 23:15:21 -0400

    Initialize a single article view as a WebView

--- app/src/main/java/com/example/ryan/d3translationapp/ArticleListViewerActivityFragment.java
+++ app/src/main/java/com/example/ryan/d3translationapp/ArticleListViewerActivityFragment.java
@@ -1,18 +1,21 @@
 package com.example.ryan.d3translationapp;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ArticleListViewerActivityFragment extends Fragment {
+public class ArticleListViewerActivityFragment extends Fragment implements View.OnClickListener {
 
 	private ListView mListView;
+	private Button button;
 
 	public ArticleListViewerActivityFragment() {
 	}
@@ -22,6 +25,15 @@ public class ArticleListViewerActivityFragment extends Fragment {
 	                         Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_article_list_viewer, container, false);
 		mListView = (ListView) view.findViewById(R.id.article_list_view);
+		button = (Button) view.findViewById(R.id.button2);
+		button.setOnClickListener(this);
 		return view;
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(getActivity(), ArticleActivity.class);
+		startActivity(intent);
 	}
 }
